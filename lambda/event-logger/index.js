9a6a5d4af84b244522f04548eecb7f45c9d9ebdc
Lorenzo Nicora <lorenzo.nicora@gmail.com>
2018-02-16 15:05:00 +0000

    Remove unused lambdas

--- lambda/event-logger/index.js
+++ lambda/event-logger/index.js
@@ -1,7 +1,9 @@
+// FOR TROUBLESHOOTING AND DEBUGGING ONLY
+//
 // Logs the event and forward request or response
-// For troubleshooting and debugging 
 
-// For Viewer Request or Origin Request triggers
+
+// Bind to Viewer Request or Origin Request triggers
 exports.requestInterceptor = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     const request = event.Records[0].cf.request;
@@ -9,7 +11,7 @@ exports.requestInterceptor = (event, context, callback) => {
     callback(null, request);
 }
 
-// For Viewer Response or Origin Response triggers
+// Bind to Viewer Response or Origin Response triggers
 exports.responseInterceptor = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     const response = event.Records[0].cf.response;
